019713fc3429009fbdace8b8f301e7ebe89e101e
Azamat-Sult <asultangareev@gmail.com>
2021-11-21 23:15:48 +0500

    0. Spring test [#6881]

---
+++ src/test/java/ru/job4j/forum/controller/ForumGetMethodTest.java
@@ -0,0 +1,107 @@
+package ru.job4j.forum.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import ru.job4j.forum.Main;
+
+@Transactional
+@SpringBootTest(classes = Main.class)
+@AutoConfigureMockMvc
+class ForumGetMethodTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    public void GetIndexPageTest() throws Exception {
+        this.mockMvc.perform(get("/index"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+    @Test
+    @WithMockUser
+    public void GetLoginPageTest() throws Exception {
+        this.mockMvc.perform(get("/login"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
+
+    @Test
+    @WithMockUser
+    public void GetRegPageTest() throws Exception {
+        this.mockMvc.perform(get("/reg"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("reg"));
+    }
+
+    @Test
+    @WithMockUser
+    public void GetShowTopicPageTest() throws Exception {
+        this.mockMvc.perform(get("/topic?id=1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("topic"));
+    }
+
+    @Test
+    @WithMockUser(roles={"MODERATOR"})
+    public void GetUpdatePostPageTest() throws Exception {
+        this.mockMvc.perform(get("/updatePost?tid=1&pid=1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("updatePost"));
+    }
+
+    @Test
+    @WithMockUser(roles={"MODERATOR"})
+    public void GetDeletePostTest() throws Exception {
+        this.mockMvc.perform(get("/deletePost?tid=1&pid=1"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/topic?id=1"));
+    }
+
+    @Test
+    @WithMockUser(roles={"USER", "MODERATOR"})
+    public void GetCreateTopicPageTest() throws Exception {
+        this.mockMvc.perform(get("/createTopic"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("createTopic"));
+    }
+
+    @Test
+    @WithMockUser(roles={"MODERATOR"})
+    public void GetUpdateTopicPageTest() throws Exception {
+        this.mockMvc.perform(get("/updateTopic?id=1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("updateTopic"));
+    }
+
+    @Test
+    @WithMockUser(roles={"MODERATOR"})
+    public void GetDeleteTopicTest() throws Exception {
+        this.mockMvc.perform(get("/deleteTopic?id=1"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+    }
+
+}

b2427c1e3ff067754af84e89b2b7c7d8319fc6a5
Azamat-Sult <asultangareev@gmail.com>
2021-11-21 17:35:54 +0500

    2. Spring boot security [#296071]

--- db/init.sql
+++ db/init.sql
@@ -1,21 +1,37 @@
-INSERT INTO authorities VALUES(1, 'ROLE_MODERATOR');
-INSERT INTO authorities VALUES(2, 'ROLE_USER');
+INSERT INTO authorities (authority) VALUES ('ROLE_MODERATOR');
+INSERT INTO authorities (authority) VALUES ('ROLE_USER');
 
-INSERT INTO users VALUES(1, 'admin', '123456', 1, true);
-INSERT INTO users VALUES(2, 'user1', '123456', 2, true);
-INSERT INTO users VALUES(3, 'user2', '123456', 2, true);
-INSERT INTO users VALUES(4, 'user3', '123456', 2, true);
+INSERT INTO users (username, enabled, password, authority_id)
+VALUES ('admin', true, '$2a$10$YBaEs/51Df1U1/MSUO/gPexO3wNK6JUYCpyifiRMwi67R8SO0XAY6',
+(select id from authorities where authority = 'ROLE_MODERATOR'));
 
-INSERT INTO topics VALUES(1, 'О чем и для чего этот форум?', 'Стартовый пост 1', '2021-11-16 19:44:58', 1);
-INSERT INTO topics VALUES(2, 'Правила для участников форума', 'Стартовый пост 2', '2021-11-17 19:44:58', 1);
-INSERT INTO topics VALUES(3, 'Все что связано с Java Core', 'Стартовый пост 3', '2021-11-18 19:44:58', 2);
-INSERT INTO topics VALUES(4, 'Все что связано с Java Collection', 'Стартовый пост 4', '2021-11-19 19:44:58', 3);
-INSERT INTO topics VALUES(5, 'Все что связано с Java Spring', 'Стартовый пост 5', '2021-11-20 19:44:58', 4);
+INSERT INTO topics (name, description, created, author_id) VALUES ('О чем и для чего этот форум?',
+ 'Стартовый пост 1', '2021-11-16 19:44:58', (select id from users where username = 'admin'));
+INSERT INTO topics (name, description, created, author_id) VALUES ('Правила для участников форума',
+ 'Стартовый пост 2', '2021-11-17 19:44:58', (select id from users where username = 'admin'));
+INSERT INTO topics (name, description, created, author_id) VALUES ('Все что связано с Java Core',
+ 'Стартовый пост 3', '2021-11-18 19:44:58', (select id from users where username = 'admin'));
+INSERT INTO topics (name, description, created, author_id) VALUES ('Все что связано с Java Collection',
+ 'Стартовый пост 4', '2021-11-19 19:44:58', (select id from users where username = 'admin'));
+INSERT INTO topics (name, description, created, author_id) VALUES ('Все что связано с Java Spring',
+ 'Стартовый пост 5', '2021-11-20 19:44:58', (select id from users where username = 'admin'));
 
-INSERT INTO posts VALUES(1, 'Post 1. Bla-bla-bla Bla-bla-bla Bla-bla-bla Bla-bla-bla', 2, '2021-11-17 19:44:58');
-INSERT INTO posts VALUES(2, 'Post 2. Bla-bla-bla Bla-bla-bla Bla-bla-bla Bla-bla-bla', 3, '2021-11-17 19:44:58');
-INSERT INTO posts VALUES(3, 'Post 3. Bla-bla-bla Bla-bla-bla Bla-bla-bla Bla-bla-bla', 4, '2021-11-17 19:44:58');
+INSERT INTO posts (text, author_id, created) VALUES ('Post 1. Bla-bla-bla Bla-bla-bla Bla-bla-bla Bla-bla-bla',
+ (select id from users where username = 'admin'), '2021-11-17 19:44:58');
+INSERT INTO posts (text, author_id, created) VALUES ('Post 2. Bla-bla-bla Bla-bla-bla Bla-bla-bla Bla-bla-bla',
+ (select id from users where username = 'admin'), '2021-11-17 19:44:58');
+INSERT INTO posts (text, author_id, created) VALUES ('Post 3. Bla-bla-bla Bla-bla-bla Bla-bla-bla Bla-bla-bla',
+ (select id from users where username = 'admin'), '2021-11-17 19:44:58');
 
-INSERT INTO topics_posts VALUES(1, 1, 1);
-INSERT INTO topics_posts VALUES(2, 1, 2);
-INSERT INTO topics_posts VALUES(3, 1, 3);
+INSERT INTO topics_posts (topic_id, posts_id) VALUES (
+(select id from topics where name = 'О чем и для чего этот форум?'),
+ (select id from posts where text = 'Post 1. Bla-bla-bla Bla-bla-bla Bla-bla-bla Bla-bla-bla')
+);
+INSERT INTO topics_posts (topic_id, posts_id) VALUES (
+(select id from topics where name = 'О чем и для чего этот форум?'),
+ (select id from posts where text = 'Post 2. Bla-bla-bla Bla-bla-bla Bla-bla-bla Bla-bla-bla')
+);
+INSERT INTO topics_posts (topic_id, posts_id) VALUES (
+(select id from topics where name = 'О чем и для чего этот форум?'),
+ (select id from posts where text = 'Post 3. Bla-bla-bla Bla-bla-bla Bla-bla-bla Bla-bla-bla')
+);
--- src/main/java/ru/job4j/forum/repository/UserRepository.java
+++ src/main/java/ru/job4j/forum/repository/UserRepository.java
@@ -7,4 +7,6 @@ public interface UserRepository extends CrudRepository<User, Long> {
 
     User findByUsername(String userName);
 
+    boolean existsByUsername(String userName);
+
 }
